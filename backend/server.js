b87b7509693fbc5a96ab5449461f652e6603444e
Lee-C-H <brisgi2@naver.com>
2020-12-25 19:27:04 +0900

    fixed

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const db = require('./db');
 const app = express();
 app.use(bodyParser.json());
 
-db.pool.query('CREATE TABLE lists (id INTEGER AUTO_INCREMENT, value Text, PRIMARY KEY (id)', function(err, result, fileds){
+db.pool.query('CREATE TABLE lists (id INTEGER AUTO_INCREMENT, value Text, PRIMARY KEY (id))', function(err, result, fileds){
     console.log('result', result);
 });
 
